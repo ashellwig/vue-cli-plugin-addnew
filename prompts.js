78b8eae3b83807654ad870d78b1a8d4666f0ae60
Ashton Scott Hellwig <ahellwig.dev@gmail.com>
2018-12-25 02:27:12 -0700

    add prompts

---
+++ prompts.js
@@ -0,0 +1,33 @@
+function isComponent (answers) {
+  return answers.action === 'addNewComponent'
+}
+
+function isView (answers) {
+  return answers.action === 'addNewView'
+}
+
+module.exports = [{
+  name: 'action',
+  message: 'What should we add?:',
+  type: 'list',
+  choices: [{
+    name: 'Component',
+    value: 'addNewComponent'
+  }, {
+    name: 'View',
+    value: 'addNewView'
+  }],
+  default: 'addNewComponent'
+}, {
+  name: 'componentName',
+  type: 'input',
+  message: 'What is the name of the new component?',
+  when: isComponent,
+  default: 'NewComponent'
+}, {
+  name: 'viewName',
+  type: 'input',
+  message: 'What is the name of the new view?',
+  when: isView,
+  default: 'NewView'
+}]
