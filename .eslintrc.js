43b22dcb616445f1df201b3954cb841444688795
Ashton Scott Hellwig <ahellwig.dev@gmail.com>
2018-12-25 01:27:32 -0700

    remove babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,4 @@
 module.exports = {
     "extends": "standard",
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "sourceType": "module",
-        "allowImportExportEverywhere": false,
-        "codeFrame": true
-    }
-    "rules": {
-        "strict": 0
-    }
+    "parser": "babel-eslint"
 };
