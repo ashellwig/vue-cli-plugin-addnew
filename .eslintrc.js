2229ae037f8a3b48b9592cb9da07011fba8c8c63
Ashton Scott Hellwig <ahellwig.dev@gmail.com>
2018-12-24 22:16:50 -0700

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "standard",
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "sourceType": "module",
+        "allowImportExportEverywhere": false,
+        "codeFrame": true
+    }
+    "rules": {
+        "strict": 0
+    }
+};
