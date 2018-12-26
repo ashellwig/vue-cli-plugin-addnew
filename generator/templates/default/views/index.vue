bcca63e845ea5ad40375274833056db4df991335
Ashton Scott Hellwig <ahellwig.dev@gmail.com>
2018-12-25 18:13:41 -0700

    add views

---
+++ generator/templates/default/views/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>
+    <%= options.componentName %>
+  </h1>
+</template>
--- generator/util/addNewView.js
+++ generator/util/addNewView.js
@@ -1,21 +1,12 @@
 const helpers = require('./helpers')
 
 function renderFiles (api, opts) {
-  // const pluginFilename = api.hasPlugin('typescript') ? 'addnew.ts' : 'addnew.js'
-  // const pluginSourceFilename = 'addnew.js'
-  // api.render({
-  //   [`./src/plugins/${pluginFilename}`]: `../templates/default/src/plugins/${pluginSourceFilename}`
-  // }, {
-  //   ...opts,
-  //   typescript: api.hasPlugin('typescript')
-  // })
-
   const viewName = opts.viewName
 
-  const viewTarget = `./src/views/${viewName}`
+  const viewTarget = `./src/views/${viewName}.vue`
 
   const files = {
-    [viewTarget]: `../template/default/src/views/${viewName}.js`
+    [viewTarget]: `../template/default/src/views/index.js`
   }
 
   api.render(files, opts)
@@ -23,18 +14,44 @@ function renderFiles (api, opts) {
 
 function addImports (api, opts) {
   const viewName = opts.viewName
-  const viewTarget = `./src/views/${viewName}`
+  const viewTarget = `./views/${viewName}.vue`
 
   helpers.updateFile(api, api.entryFile, lines => {
-    const vueImportIndex = lines.findIndex(line => line.match(/^import Vue/))
+    const lastImportIndex = lines.findIndex(line => line.match(/^import Vue/))
 
-    lines.splice(vueImportIndex + 1, 0, `import '${viewTarget}'`)
+    lines.splice(lastImportIndex + 1, 0, `import ${viewName} from '${viewTarget}'`)
 
     return lines
   })
 }
 
+function addPathToRouter (api, opts) {
+  const viewName = opts.viewName
+  const viewPath = opts.path
+
+  const fs = require('fs')
+  const routerPath = api.resolve('./src/router.js')
+  opts.router = fs.existsSync(routerPath)
+
+  if (opts.router) {
+    helpers.updateFile(api, routerPath, lines => {
+      const lastImportIndex = lines.findIndex(line => lines.match(/^import/))
+      const lastRoute = lines.findIndex(line => line.match(/^routes: \[/))
+
+      lines.splice(lastImportIndex + 1, 0, `
+      const ${viewName}Route = {
+        path: ${viewPath},
+        name: ${viewName},
+        component: viewName
+      }`)
+
+      lines.splice(lastRoute + 1, 0, `${viewName}Route`)
+    })
+  }
+}
+
 module.exports = {
   renderFiles,
+  addPathToRouter,
   addImports
 }
