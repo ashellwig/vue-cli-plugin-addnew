ed7a468363afdf1219fec79000168dc3c5902304
Ashton Scott Hellwig <ahellwig.dev@gmail.com>
2018-12-27 23:54:22 -0700

    register anew:comp as command

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = (api, opts) => {}
+module.exports = (api, opts) => {
+  api.registerCommand('anew:comp', {
+    description: 'Create and globally register a new component',
+    usage: 'vue-cli-service anew:comp'
+  }, args => {
+    require('./generator/lib').addANewComponent(api, opts)
+  })
+}
