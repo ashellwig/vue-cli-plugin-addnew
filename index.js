2b969f826cd64178c022cc7369b0b364eb2fc588
Ashton Scott Hellwig <ahellwig.dev@gmail.com>
2018-12-29 01:18:07 -0700

    removed commands for now

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 module.exports = (api, opts) => {
-  api.registerCommand('anew:comp', {
-    description: 'Create and globally register a new component',
-    usage: 'vue-cli-service anew:comp'
-  }, args => {
-    require('./generator/lib').addANewComponent(api, opts)
-  })
+  // api.registerCommand('anew:comp', {
+  //   description: 'Create and globally register a new component',
+  //   usage: 'vue-cli-service anew:comp [options]',
+  //   options: {
+  //     '--componentName \'[name]\'': 'Name of component to be added'
+  //   }
+  // }, args => {
+  //   require('./generator/lib').addANewComponent(api, opts)
+  // })
 }
